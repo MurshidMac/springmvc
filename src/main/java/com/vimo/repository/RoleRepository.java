f503929469ba81e63bd129bb8486118fda9edc95
Murshid Hassen <mhassen@zaizi.com>
2019-06-19 17:19:45 +0530

    mvn changes done
    mvn changes done

---
+++ src/main/java/com/vimo/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.vimo.repository;
+
+import com.vimo.model.Role;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository("roleRepository")
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Role findByRole(String role);
+}
